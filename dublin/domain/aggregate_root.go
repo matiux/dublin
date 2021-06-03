8d7727d60eab50d5475dba365616906858d223ed
Matteo <m.galacci@gmail.com>
2021-06-04 00:27:18 +0200

    feat: add iterator

--- dublin/domain/aggregate_root.go
+++ dublin/domain/aggregate_root.go
@@ -3,5 +3,7 @@ package domain
 // AggregateRoot represents entities that are an aggregate root.
 type AggregateRoot interface {
 
+	getUncommittedEvents() EventStream
+
 	getAggregateRootId() string
 }
--- dublin/domain/event_stream.go
+++ dublin/domain/event_stream.go
@@ -1,6 +1,14 @@
 package domain
 
-// EventStream a stream of DomainEventMessages in sequence.
+// EventStream a stream of Message(S) in sequence.
 type EventStream struct {
-	
+	events []Message
+}
+
+func (es EventStream) getIterator() StreamIterator {
+	return NewStreamIterator(es.events)
+}
+
+func NewEventStream(events []Message) EventStream {
+	return EventStream{events}
 }
