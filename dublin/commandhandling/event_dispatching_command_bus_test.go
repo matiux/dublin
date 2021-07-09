f1b1feb660a77d832d1faaddb9eb16f557375f97
Matteo <m.galacci@gmail.com>
2021-07-09 15:15:26 +0200

    wip: event dispatching command bus

--- dublin/commandhandling/event_dispatching_command_bus_test.go
+++ dublin/commandhandling/event_dispatching_command_bus_test.go
@@ -1,6 +1,7 @@
 package commandhandling
 
 import (
+	"fmt"
 	"github.com/golang/mock/gomock"
 	"github.com/matiux/dublin/dublin/eventdispatcher"
 	"testing"
@@ -9,8 +10,7 @@ import (
 var eventDispatcher eventdispatcher.EventDispatcher
 var command Command
 var commandBus CommandBus
-
-//var subscriber CommandHandler
+var subscriber CommandHandler
 var eventDispatchingCommandBus *EventDispatchingCommandBus
 
 //func TestMain(m *testing.M) {
@@ -26,7 +26,7 @@ func setup(t *testing.T) {
 
 	eventDispatcher = eventdispatcher.NewMockEventDispatcher(ctrl)
 	commandBus = NewMockCommandBus(ctrl)
-	//subscriber = NewMockCommandHandler(ctrl)
+	subscriber = NewMockCommandHandler(ctrl)
 	command = TestCommand{Name: "matiux"}
 	eventDispatchingCommandBus = NewEventDispatchingCommandBus(commandBus, eventDispatcher)
 }
@@ -52,4 +52,49 @@ func Test_it_dispatches_the_success_event(t *testing.T) {
 	)
 
 	_ = eventDispatchingCommandBus.Dispatch(command)
+}
+
+func Test_it_dispatches_the_failure_event_and_forwards_the_exception(t *testing.T) {
+
+	setup(t)
+
+	myError := fmt.Errorf("an myError")
+
+	gomock.InOrder(
+		commandBus.(*MockCommandBus).EXPECT().Dispatch(command).Return(myError).Times(1),
+		eventDispatcher.(*eventdispatcher.MockEventDispatcher).
+			EXPECT().
+			Dispatch(
+				EventCommandFailure,
+				map[string]interface{}{"command": command, "exception": myError},
+			).Times(1),
+	)
+
+	_ = eventDispatchingCommandBus.Dispatch(command)
+}
+
+//func Test_it_forwards_the_dispatched_command(t *testing.T) {
+//
+//	setup(t)
+//	gomock.InOrder(
+//		commandBus.(*MockCommandBus).EXPECT().Dispatch(command).Times(1),
+//		eventDispatcher.(*eventdispatcher.MockEventDispatcher).
+//			EXPECT().
+//			Dispatch(
+//				EventCommandSuccess,
+//				map[string]interface{}{
+//					"command": command,
+//				},
+//			),
+//	)
+//	_ = eventDispatchingCommandBus.Dispatch(command)
+//}
+
+func Test_it_forwards_the_subscriber(t *testing.T) {
+
+	setup(t)
+
+	commandBus.(*MockCommandBus).EXPECT().Subscribe(subscriber).Times(1)
+
+	eventDispatchingCommandBus.Subscribe(subscriber)
 }
