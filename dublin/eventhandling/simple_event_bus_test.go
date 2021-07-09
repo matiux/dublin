ccace842f3fdf8da70b96352c2f63cc23312f106
Matteo <m.galacci@gmail.com>
2021-07-09 17:03:02 +0200

    wip: event bus

--- dublin/eventhandling/simple_event_bus_test.go
+++ dublin/eventhandling/simple_event_bus_test.go
@@ -1,1 +1,19 @@
 package eventhandling
+
+import "testing"
+
+var eventBus *SimpleEventBus
+
+func setup(t *testing.T) {
+
+	eventBus = NewSimpleEventBus()
+}
+
+func Test_it_subscribes_an_event_listener(t *testing.T) {
+
+	setup(t)
+}
+
+type TestEvent struct {
+	Name string
+}
